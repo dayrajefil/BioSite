9258049e9459b1e57a2abc4631c6bdff61c4ca67
dayrajefil <dayra.manicoba@imaginedone.com.br>
2022-12-27 22:09:55 -0300

    [0003] adding packages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
+//= require jquery
+//= require notifyjs/dist/notify.js
+//= require bootstrap/dist/js/bootstrap
+//= require popper.js/dist/popper
